3f58d19ca55b5a4557fc84fe6640202c12b9cff5
Shu Ishikura <i.shuuuu0723@gmail.com>
2020-10-10 21:35:18 +0900

    textの感情評価まで作成

--- src/views/Twitter.vue
+++ src/views/Twitter.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <v-btn @click="getTwitterPosts()">getTwitterPosts</v-btn>
+    <v-btn @click="analyseanalyzeSentiment()">analyseanalyzeSentiment</v-btn>
   </div>
 </template>
 
@@ -8,18 +9,59 @@
 export default {
   name: "Twitter",
   data() {
-    return {};
+    return {
+      linkedText: "",
+      // flower : {
+      //   id:"1",
+      //   name:"バラ",
+      //   photo:".path",
+      //   description:""
+      // }
+    };
   },
   methods: {
     getTwitterPosts() {
-      const axios = require('axios');
-      axios.get('https://us-central1-kotobana-twitter.cloudfunctions.net/timeline')
-      .then(response => {
-          console.log('status:', response.status); // 200
-         console.log('body:', response.data);     // response body.
-      }).catch(err => {
-          console.log('err:', err);
-      });
+      const axios = require("axios");
+      axios
+        .get("https://us-central1-kotobana-twitter.cloudfunctions.net/timeline")
+        .then((response) => {
+          // console.log("status:", response.status); // 200
+          // console.log("body:", response.data); // response body.
+          this.linkedText = response.data.reduce((accumulator, currentValue) => {
+            // console.log("currentValue",currentValue);
+            return accumulator + currentValue.text;
+          }, "");
+          console.log("text:", this.linkedText);
+        })
+        .catch((err) => {
+          console.log("err:", err);
+        });
+      console.log("plase write javascript logic here");
+    },
+    analyseanalyzeSentiment() {
+      const axios = require("axios");
+      const apiKey = "AIzaSyBJMd-cy43E8GRKkOIP5EN0nobvahp2-v0";
+      const URL =
+        "https://language.googleapis.com/v1beta2/documents:analyzeSentiment?key=" +
+        apiKey;
+      const Headers = {
+        "Content-Type": "application/json",
+      };
+      const data = {
+        document: {
+          type: "PLAIN_TEXT",
+          content: this.linkedText,
+        },
+      };
+      axios
+        .post(URL, data, Headers)
+        .then((response) => {
+          console.log("status:", response.status); // 200
+          console.log("body:", response.data); // response body.
+        })
+        .catch((err) => {
+          console.log("err:", err);
+        });
       console.log("plase write javascript logic here");
     },
   },
