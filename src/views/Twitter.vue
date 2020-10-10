3ce5781b0a25fa5c0223cb268f81713b6b4ccdb1
tyutat <ytsunoda2511@gmail.com>
2020-10-10 19:10:17 +0900

    tweetしたデータを取得する関数を追加

--- src/views/Twitter.vue
+++ src/views/Twitter.vue
@@ -12,6 +12,14 @@ export default {
   },
   methods: {
     getTwitterPosts() {
+      const axios = require('axios');
+      axios.get('https://us-central1-kotobana-twitter.cloudfunctions.net/timeline')
+      .then(response => {
+          console.log('status:', response.status); // 200
+         console.log('body:', response.data);     // response body.
+      }).catch(err => {
+          console.log('err:', err);
+      });
       console.log("plase write javascript logic here");
     },
   },
