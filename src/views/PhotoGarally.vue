75ba559beb521262645e903e24c01194a4b73e96
kazuaki takahashi <kazuaki.takahashi3@gmail.com>
2020-10-10 21:20:44 +0900

    add form

--- src/views/PhotoGarally.vue
+++ src/views/PhotoGarally.vue
@@ -1,43 +1,49 @@
 <template>
-  <v-row>
-    <v-img
-      src="https://bad.src/not/valid"
-      lazy-src="https://picsum.photos/id/11/100/60"
-      max-width="500"
-      max-height="300"
-    >
-      <template v-slot:placeholder>
-        <v-row
-          class="fill-height ma-0"
-          align="center"
-          justify="center"
-        >
-          <v-progress-circular
-            indeterminate
-            color="grey lighten-5"
-          ></v-progress-circular>
-        </v-row>
-      </template>
-    </v-img>
-    
-    <v-col
-      v-for="n in 9"
-      :key="n"
-      class="d-flex child-flex"
-      cols="4"
-    >
+  <div>
+    <v-row>
+      <v-col cols="10" offset-md="1">
+        <v-form ref="form">
+          <v-text-field label="First name"></v-text-field>
+        </v-form>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-card>
+          <v-img
+            src="https://picsum.photos/500/300?image=1"
+            class="white--text align-end"
+            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+          >
+          </v-img>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+
+            <v-btn icon>
+              <v-icon>mdi-heart</v-icon>
+            </v-btn>
+
+            <v-btn icon>
+              <v-icon>mdi-bookmark</v-icon>
+            </v-btn>
+
+            <v-btn icon>
+              <v-icon>mdi-share-variant</v-icon>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
       <v-img
-        :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
-        :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
-        aspect-ratio="1"
-        class="grey lighten-2"
+        src="https://bad.src/not/valid"
+        lazy-src="https://picsum.photos/id/11/100/60"
+        max-width="500"
+        max-height="300"
       >
         <template v-slot:placeholder>
-          <v-row
-            class="fill-height ma-0"
-            align="center"
-            justify="center"
-          >
+          <v-row class="fill-height ma-0" align="center" justify="center">
             <v-progress-circular
               indeterminate
               color="grey lighten-5"
@@ -45,8 +51,26 @@
           </v-row>
         </template>
       </v-img>
-    </v-col>
-  </v-row>
+
+      <v-col v-for="n in 9" :key="n" class="d-flex child-flex" cols="4">
+        <v-img
+          :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+          :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
+          aspect-ratio="1"
+          class="grey lighten-2"
+        >
+          <template v-slot:placeholder>
+            <v-row class="fill-height ma-0" align="center" justify="center">
+              <v-progress-circular
+                indeterminate
+                color="grey lighten-5"
+              ></v-progress-circular>
+            </v-row>
+          </template>
+        </v-img>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
