d0e666c2b1146b9ecb479f4a850825277f6ec2b3
kazuaki takahashi <kazuaki.takahashi3@gmail.com>
2020-10-10 19:14:15 +0900

    add bottm

---
+++ src/views/Profile.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    Profile
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Profile",
+  data() {
+    return {};
+  },
+  methods: {
+    getTwitterPosts() {
+      console.log("plase write javascript logic here");
+    },
+  },
+};
+</script>
